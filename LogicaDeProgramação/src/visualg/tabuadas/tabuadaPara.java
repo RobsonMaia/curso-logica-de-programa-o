6cea1440c37bfdf9a214634dd579d719639c7670
robson <robson@robson-Default-string>
2024-07-06 12:43:00 -0300

    tabuada para

---
+++ LogicaDeProgramação/src/visualg/tabuadas/tabuadaPara.java
@@ -0,0 +1,15 @@
+Algoritmo "tabuada_Para"
+
+
+Var
+
+      N, I, produto: inteiro
+
+Inicio
+       escreval("Qual tabuada você quer ver? ")
+       leia (N)
+       para I de 0 ate 10 faca
+            produto <- N * I
+            escreval(N," X ",I, " = ",produto)
+       fimpara
+Fimalgoritmo
