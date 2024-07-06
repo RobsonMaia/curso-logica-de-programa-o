de342fcd29febfbf7cf18751246832416ae86d56
robson <robson@robson-Default-string>
2024-07-06 12:41:21 -0300

    tabuada enquanto

---
+++ LogicaDeProgramação/src/visualg/tabuadas/tabuadaEnquanto.java
@@ -0,0 +1,18 @@
+Algoritmo "tabuada_Enquanto"
+
+
+Var
+
+      N, I, produto: inteiro
+
+Inicio
+       escreval("Qual tabuada você quer ver? ")
+       leia (N)
+       I <- 0
+       enquanto I <= 10 faca
+            produto <- N * I
+            escreval(N," X ",I, " = ",produto)
+            I <- I + 1
+       fimenquanto
+
+Fimalgorit
