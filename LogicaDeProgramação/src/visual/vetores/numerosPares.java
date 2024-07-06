b742745b8b2c4948dc68f52a823fcf627a6c2a52
robson <robson@robson-Default-string>
2024-07-06 13:51:47 -0300

    numeros pares

---
+++ LogicaDeProgramação/src/visual/vetores/numerosPares.java
@@ -0,0 +1,30 @@
+Algoritmo "numeros pares"
+
+
+Var
+
+      N, i, cont : inteiro
+      nd : vetor [0..99] de inteiro
+
+Inicio
+       escreval("Quantas numeros serao digitadas?")
+       leia (N)
+
+       para i de 0 ate N-1 faca
+            escreval("Digite um numero: " )
+            leia (nd[i])
+      fimpara
+      escreval
+      
+      cont <- 0
+      escreval ("NUMEROS PARES")
+      para i de 0 ate N-1 faca
+           se  nd[i] % 2 = 0 entao
+               cont <- + 1
+               escreva (nd[i])
+           fimse
+      fimpara
+      escreval
+      escreval("QUANTIDADE DE PARES = ", cont)
+
+Fimalgoritmo
