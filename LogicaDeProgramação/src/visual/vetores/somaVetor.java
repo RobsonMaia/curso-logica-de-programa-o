ece40eaac72431f55183413d6567235d7cbe83ae
robson <robson@robson-Default-string>
2024-07-06 12:46:28 -0300

    vetores

---
+++ LogicaDeProgramação/src/visual/vetores/somaVetor.java
@@ -0,0 +1,35 @@
+Algoritmo "teste vetor soma vetor"
+
+
+Var
+
+      N, i : inteiro
+      soma, media: real
+      vet : vetor [0..9] de real
+
+Inicio
+       escreval("Quantos numeros voce vai digitar?")
+       leia (N)
+
+       para i de 0 ate N-1 faca
+            escreval("Digite um numero: ")
+            leia (vet[i])
+       fimpara
+
+       escreva("VALORES: ")
+       para i de 0 ate N-1 faca
+            escreva(vet[i]:3:1, " ")
+       fimpara
+
+       escreval
+       soma <- 0
+       para i de 0 ate N-1 faca
+            soma <- soma + vet[i]
+       fimpara
+       escreval ("SOMA: ", soma)
+       
+       media <- soma / N
+       
+       escreval("MEDIA: ", media)
+       
+Fimalgoritmo
---
+++ LogicaDeProgramação/src/visual/vetores/testeVetorNegativos.java
@@ -0,0 +1,26 @@
+Algoritmo "teste vetor negativos"
+
+
+Var
+
+      N, i: inteiro
+      vet : vetor [0..9] de inteiro
+
+Inicio
+       escreval("Quantos numeros voce vai digitar?")
+       leia (N)
+
+       para i de 0 ate N-1 faca
+            escreval("Digite um numero: ")
+            leia (vet[i])
+       fimpara
+
+       escreval("NUMEROS NEGATIVOS:")
+
+       para i de 0 ate N-1 faca
+            se vet[i] < 0 entao
+               escreval(vet[i])
+            fimse
+       fimpara
+
+Fimalgoritmo
